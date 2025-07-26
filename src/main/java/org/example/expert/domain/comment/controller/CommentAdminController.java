4309abc871342d1aac2b22568b07101a9ec2a2cc
pigglehyun <sarah0661@pusan.ac.kr>
2025-07-26 14:09:39 +0900

    4-1. refactor: change return type for AOP test

--- src/main/java/org/example/expert/domain/comment/controller/CommentAdminController.java
+++ src/main/java/org/example/expert/domain/comment/controller/CommentAdminController.java
@@ -1,7 +1,9 @@
 package org.example.expert.domain.comment.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.expert.aspect.AdminLogging;
 import org.example.expert.domain.comment.service.CommentAdminService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +14,10 @@ public class CommentAdminController {
 
     private final CommentAdminService commentAdminService;
 
+    @AdminLogging
     @DeleteMapping("/admin/comments/{commentId}")
-    public void deleteComment(@PathVariable long commentId) {
+    public ResponseEntity<?> deleteComment(@PathVariable long commentId) {
         commentAdminService.deleteComment(commentId);
+        return ResponseEntity.ok("comment 삭제 완료");
     }
 }
--- src/main/java/org/example/expert/domain/user/controller/UserAdminController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserAdminController.java
@@ -1,8 +1,11 @@
 package org.example.expert.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.example.expert.aspect.AdminLogging;
 import org.example.expert.domain.user.dto.request.UserRoleChangeRequest;
 import org.example.expert.domain.user.service.UserAdminService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserAdminController {
 
     private final UserAdminService userAdminService;
-
+    @AdminLogging
     @PatchMapping("/admin/users/{userId}")
-    public void changeUserRole(@PathVariable long userId, @RequestBody UserRoleChangeRequest userRoleChangeRequest) {
+    public ResponseEntity<?> changeUserRole(@PathVariable long userId, @RequestBody UserRoleChangeRequest userRoleChangeRequest) {
         userAdminService.changeUserRole(userId, userRoleChangeRequest);
+        return ResponseEntity.ok("사용자 권한 변경 완료");
     }
 }
